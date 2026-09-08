81bcf560726e5852c60c1093dc0ac8c2a9d1ff7a
TKlahr <152762116+TKlahr@users.noreply.github.com>
2026-09-08 12:02:07 -0700

    Fix formatting and comments in birthday calculator script

--- Assignments/Lab-Linux Shell Script/100th-Birthday-Calc.sh
+++ Assignments/Lab-Linux Shell Script/100th-Birthday-Calc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Please enter your name:"    #Ask user for name
-read name    #store input in name
+echo "Please enter your name:"    # Ask user for name
+read name    # store input in name
 
-echo "Hello, $name! How old are you?"    #Ask user for age
+echo "Hello, $name! How old are you?"    # Ask user for age
 read age    # Store input in age
 
-currentyear=$(date +%Y)    #Displays just the year of current date on the system. Y=4, y=2
-yearsleft=$((100-age))    #Calculates how many years until user is 100
-hundredyear=$((currentyear + yearsleft))    #Calculates what year the user will turn 100
+currentyear=$(date +%Y)    # Displays just the year of current date on the system. Y=4, y=2
+yearsleft=$((100-age))    # Calculates how many years until user is 100
+hundredyear=$((currentyear + yearsleft))    # Calculates what year the user will turn 100
 
-echo "$name, you will be 100 years old in the year $hundredyear."    #Tells the user what year they will turn 100
+echo "$name, you will be 100 years old in the year $hundredyear."    # Tells the user what year they will turn 100
